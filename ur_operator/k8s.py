b906cc224eae0fc3c283a20d009e94a429d1b916
Evan-TwinHats <evan@twinhats.com>
2025-01-31 11:56:36 -0500

    Update k8s.py

--- ur_operator/k8s.py
+++ ur_operator/k8s.py
@@ -62,8 +62,8 @@ class K8s:
     def list_k8s_crd_obj(self, namespace):
         return self.custom_objects_api.list_namespaced_custom_object(
             group=constants.GROUP,
-            version=MonitorV1Beta1.version,
-            plural=MonitorV1Beta1.plural,
+            version=constants.VERSION,
+            plural=constants.PLURAL,
             namespace=namespace
         )
 
